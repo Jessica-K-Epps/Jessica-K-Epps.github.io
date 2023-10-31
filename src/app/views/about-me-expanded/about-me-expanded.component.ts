c90695981c1c8f244b43bb63aea6b2b0a0b9582f
James-M-Boyette <James.M.84.Boyette@gmail.com>
2023-10-31 19:04:55 -0400

    add button

--- src/app/views/about-me-expanded/about-me-expanded.component.ts
+++ src/app/views/about-me-expanded/about-me-expanded.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-me-expanded',
   templateUrl: './about-me-expanded.component.html',
-  styleUrls: ['./about-me-expanded.component.scss']
+  styleUrls: ['./about-me-expanded.component.scss'],
 })
 export class AboutMeExpandedComponent {
+  constructor(private router: Router) {}
 
+  navigateToLandingPage() {
+    console.log('Navigating back to Landing Page');
+    this.router.navigate(['/']);
+  }
 }
