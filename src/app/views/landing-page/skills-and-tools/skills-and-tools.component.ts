696e3a75e8744be7cc669f069164393fe59f2b69
James-M-Boyette <James.M.84.Boyette@gmail.com>
2023-11-14 19:33:23 -0500

    re-style 'skills' buttons for mobile

--- src/app/views/landing-page/skills-and-tools/skills-and-tools.component.ts
+++ src/app/views/landing-page/skills-and-tools/skills-and-tools.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import skillsData from './skills.json';
+
+interface Skill {
+  name: string;
+  category: string;
+  highlighted: boolean;
+}
+
 @Component({
   selector: 'app-skills-and-tools',
   templateUrl: './skills-and-tools.component.html',
-  styleUrls: ['./skills-and-tools.component.scss']
+  styleUrls: ['./skills-and-tools.component.scss'],
 })
 export class SkillsAndToolsComponent {
+  public skills: Skill[] = skillsData;
+  public categories: string[];
 
+  constructor() {
+    // Extract unique categories
+    const uniqueCategories = new Set(
+      this.skills.map((skill) => skill.category)
+    );
+    this.categories = Array.from(uniqueCategories);
+  }
+
+  toggleHighlightByCategory(category: string) {
+    this.skills.forEach((skill) => {
+      if (skill.category === category) {
+        skill.highlighted = !skill.highlighted;
+      }
+    });
+  }
+
+  toggleHighlight(skill: Skill) {
+    skill.highlighted = !skill.highlighted;
+  }
+
+  onClickSelectSkill(selectedSkill: string) {
+    console.log('selectedSkill: ', selectedSkill);
+  }
 }
